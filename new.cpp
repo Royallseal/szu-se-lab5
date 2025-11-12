aa364e0bd6c7b91013006139a7ebeb91aa734754
O-utIn <2022044002@email.szu.edu.cn>
2025-11-12 10:25:06 +0800

    second version

--- new.cpp
+++ new.cpp
@@ -21,39 +21,7 @@ int main() {
     int N;
     cin >> N;
 
-    while(N--) {
-        string word;
-        cin >> word;
-
-        int count[26] = {0};
-
-        for (char c : word) {
-            if (c >= 'a' && c <= 'z') {
-                count[c - 'a']++;
-            } else if (c >= 'A' && c <= 'Z') {
-                count[c - 'A']++;
-            }
-        }
-
-        int maxn = 0, minn = 1000000;
-
-        for (int j = 0; j < 26; j++) {
-            if (count[j] > maxn) {
-                maxn = count[j];
-            }
-            if (count[j] < minn && count[j]!= 0) {
-                minn = count[j];
-            }
-        }
-
-        if (isPrime(maxn - minn)) {
-            cout << "Lucky Word" << endl;
-            cout << maxn - minn << endl;
-        } else {
-            cout << "No Answer" << endl;
-            cout << 0 << endl;
-        }
-    }
+    
 
     return 0;
 }
